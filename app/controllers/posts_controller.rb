82946475204f5d82bea2c91c7c8abafca8641600
entcheva <christina.entcheva@gmail.com>
2016-12-30 17:17:02 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,4 +18,26 @@ class PostsController < ApplicationController
 	  @post.save
 	  redirect_to post_path(@post)
 	end
+
+  def edit
+    # show edit form for item with :id
+		@post = Post.find(params[:id])
+  end
+
+  def update
+    # update item with :id
+	  @post = Post.find(params[:id])
+	  @post.update(title: params[:title], description: params[:description])
+	  redirect_to post_path(@post)
+  end
+
+  def delete
+    # show delete form for item with :id
+  end
+
+  def destroy
+    # delete item with :id
+  end
+
+
 end
